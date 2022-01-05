f7282a0f7fda3af79d519c740ee5be6d9d49b56e
Sean Schwabenlander <sean@schwabendesign.com>
2022-01-04 20:14:50 -0600

    Added meal item and meal summary components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,14 +1,18 @@
 import { DUMMY_MEALS } from './dummy-meals';
 import styles from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
-    <li>{meal.name}</li>;
-  });
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem meal={meal} key={meal.id} />
+  ));
 
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import styles from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+
+const MealItem = (props) => {
+  const price = `$${props.meal.price.toFixed(2)}`;
+
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3>{props.meal.name}</h3>
+        <div className={styles.description}>{props.meal.description}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.meal.id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
