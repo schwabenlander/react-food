faac3ede47499895d5131e47b40a633b64ffa0f0
Sean Schwabenlander <sean@schwabendesign.com>
2022-01-04 20:40:06 -0600

    Added cart component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import styles from './Modal.module.css';
+import { Fragment } from 'react';
+import { createPortal } from 'react-dom';
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
