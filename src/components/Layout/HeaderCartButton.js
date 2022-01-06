62785339c6080d6a738b7e3390fef3cf6b2e658a
Sean Schwabenlander <sean@schwabendesign.com>
2022-01-05 20:19:48 -0600

    Added an animation effect when the cart amount is updated

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,35 @@
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
-import cartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [isButtonAminated, setIsButtonAnimated] = useState(false);
   const cartContext = useContext(CartContext);
+  const buttonClasses = `${styles.button} ${
+    isButtonAminated ? styles.bump : ''
+  }`;
 
   const numberOfCartItems = cartContext.items.reduce(
     (count, item) => count + item.amount,
     0
   );
 
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+
+    setIsButtonAnimated(true);
+    const timer = setTimeout(() => setIsButtonAnimated(false), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
